dab87bacc6146177f16c0dd9cbc8e673af38a8fc
Jee Yoon Lee <jlee66@wellesley.edu>
2018-06-06 10:01:48 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,13 +1,14 @@
 class Person
+  def talk
+    puts "Hello World!"
+  end
+
+  def walk
+    puts "The Person is walking"
+  end
 end
 
-def talk
-  puts "Hello World!"
-end
 
-def walk
-  puts "The Person is walking"
-end
 
 ada = Person.new
 ada.talk
